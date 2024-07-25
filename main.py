81e7545999ace2f647ea972d178bb92f2caa6d74
Yves Kleny <48736725+yveskleny@users.noreply.github.com>
2024-07-25 08:50:37 -0300

    main.py

--- main.py
+++ main.py
@@ -102,9 +102,9 @@ import psycopg2
 
 con = psycopg2.connect(
     host="127.0.0.1",
-    port="5432",
-    user="postgres",
-    password="y3fmY#FM",
+    port="",
+    user="",
+    password="",
     database="scrape_demo"
 )
 
